910d53d8433b2c3df68e8fd76e75d4e6aa76a913
Jordan <jordan.richards@kaust.edu.sa>
2022-05-11 17:31:35 +0300

    Added US data rda + documentation

--- R/bGEVPP_NN.R
+++ R/bGEVPP_NN.R
@@ -71,11 +71,11 @@
 #' # Build and train a simple MLP for toy data
 #'
 #' # Create  predictors
-#' preds<-rnorm(12800)
+#' preds<-rnorm(128000)
 #'
 #' #Re-shape to a 4d array. First dimension corresponds to observations,
 #' #last to the different components of the predictor set
-#' dim(preds)=c(20,8,8,10) #We have ten predictors
+#' dim(preds)=c(200,8,8,10) #We have ten predictors
 #'
 #' #Split predictors into linear, additive and nn. Different for the location and scale parameters.
 #' X_train_nn_q=preds[,,,1:5] #Five nn predictors for q_\alpha
@@ -95,37 +95,45 @@
 #'
 #' # Additive contribution
 #' m_A_1 = 0.1*X_train_add_q[,,,1]^3+0.2*X_train_add_q[,,,1]-
-#' 0.1*X_train_add_q[,,,2]^3+0.5*X_train_add_q[,,,2]^2-0.2*X_train_add_q[,,,3]^3
+#'   0.1*X_train_add_q[,,,2]^3+0.5*X_train_add_q[,,,2]^2-0.2*X_train_add_q[,,,3]^3
 #'
 #' #Non-additive contribution - to be estimated by NN
 #' m_N_1 = exp(-3+X_train_nn_q[,,,2]+X_train_nn_q[,,,3])+
 #'   sin(X_train_nn_q[,,,1]-X_train_nn_q[,,,2])*(X_train_nn_q[,,,1]+X_train_nn_q[,,,2])-
 #'   cos(X_train_nn_q[,,,3]-X_train_nn_q[,,,4])*(X_train_nn_q[,,,2]+X_train_nn_q[,,,5])
 #'
-#' q_alpha=3+m_L_1+m_A_1+m_N_1 #Identity link
+#' q_alpha=1+m_L_1+m_A_1+m_N_1 #Identity link
 #'
 #' #Contribution to scale parameter
 #' #Linear contribution
-#' m_L_2 = 0.3*X_train_lin_s[,,,1]+0.6*X_train_lin_s[,,,2]+0.1*X_train_lin_s[,,,3]-
-#' 0.2*X_train_lin_s[,,,4]+0.5*X_train_lin_s[,,,5]
+#' m_L_2 = 0.2*X_train_lin_s[,,,1]+0.6*X_train_lin_s[,,,2]+0.1*X_train_lin_s[,,,3]-
+#'   0.2*X_train_lin_s[,,,4]+0.5*X_train_lin_s[,,,5]
 #'
 #' # Additive contribution
 #' m_A_2 = 0.1*X_train_add_s[,,,1]^2+0.2*X_train_add_s[,,,1]-0.2*X_train_add_s[,,,2]^2+
-#' 0.1*X_train_add_s[,,,2]^3
+#'   0.1*X_train_add_s[,,,2]^3
 #'
 #' #Non-additive contribution - to be estimated by NN
 #' m_N_2 = exp(-3+X_train_nn_s[,,,2]+X_train_nn_s[,,,3])+
 #'   sin(X_train_nn_s[,,,1]-X_train_nn_s[,,,2])*(X_train_nn_s[,,,1]+X_train_nn_s[,,,2])
 #'
-#' s_beta=exp(-1+m_L_2+m_A_2+m_N_2) #Exponential link
+#' s_beta=0.5*exp(-2+m_L_2+m_A_2+m_N_2) #Exponential link
 #'
-#' xi=0.2 # Set xi
+#' xi=0.1 # Set xi
 #'
 #' theta=array(dim=c(dim(s_beta),3))
 #' theta[,,,1]=q_alpha; theta[,,,2] = s_beta; theta[,,,3]=xi
-#' #We simulate normal data and estimate the median, i.e., the 50% quantile or mean, as the form for this is known
+#' #We simulate data from the extreme value point process model with u take as the 80% quantile
 #'
-#' Y=apply(theta,1:3,function(x) rnorm(1,mean=x[1],x[2]))
+#' u<-apply(theta,1:3,function(x) qPP(prob=0.8,loc=x[1],scale=x[2],xi=x[3],re.par = T)) #Gives the 80% quantile of Y
+#'
+#' Y=apply(theta,1:3,function(x) rPP(1,u.prob=0.8,loc=x[1],scale=x[2],xi=x[3],re.par=T)) #Simulate from re-parametrised point process model using same u as given above
+#'
+#' # Note that the point process model is only valid for Y > u. If Y < u, then rPP gives NA.
+#' # We can set NA values to some c < u as these do not contribute to model fitting.
+#' Y[is.na(Y)]=u[is.na(Y)]-1
+#'
+#'
 #'
 #' #Create training and test, respectively.
 #' #We mask 20% of the Y values and use this for validation/testing.
@@ -142,15 +150,15 @@
 #' #To build a model with an additive component, we require an array of evaluations of
 #' #the basis functions for each pre-specified knot and entry to X_train_add_q and X_train_add_s
 #'
-#' rad=function(x,c){ #Define a basis function. Here we use the radial bases
+#'rad=function(x,c){ #Define a basis function. Here we use the radial bases
 #'   out=abs(x-c)^2*log(abs(x-c))
 #'   out[(x-c)==0]=0
-#'   return(out)
+#'  return(out)
 #' }
 #'
-#' n.knot.q = 5; n.knot.s = 4 # set number of knots.
-#' #Must be the same for each additive predictor,
-#' #but can differ between the parameters q_\alpha and s_\beta
+#'n.knot.q = 5; n.knot.s = 4 # set number of knots.
+#'#Must be the same for each additive predictor,
+#'#but can differ between the parameters q_\alpha and s_\beta
 #'
 #' #Get knots for q_\alpha predictors
 #' knots.q=matrix(nrow=dim(X_train_add_q)[4],ncol=n.knot.q)
@@ -180,34 +188,27 @@
 #'
 #' #lin+GAM+NN models defined for both location and scale parameters
 #' X_train_q=list("X_train_nn_q"=X_train_nn_q, "X_train_lin_q"=X_train_lin_q,
-#' "X_train_add_basis_q"=X_train_add_basis_q) #Predictors for q_\alpha
+#'                "X_train_add_basis_q"=X_train_add_basis_q) #Predictors for q_\alpha
 #' X_train_s=list("X_train_nn_s"=X_train_nn_s, "X_train_lin_s"=X_train_lin_s,
-#' "X_train_add_basis_s"=X_train_add_basis_s) #Predictors for s_\beta
+#'                "X_train_add_basis_s"=X_train_add_basis_s) #Predictors for s_\beta
 #'
-#' #We first use train_quant_NN to estimate the 80% quantile whih will be used as u_train for bGEVPP.NN.train
-#' #A simple MLP with no linear or additive components is used
+#' #We treat u as fixed and known. In an application, u can be estimated using quant.NN.train.
 #'
-#' model<-train_quant_NN(Y_train, Y_test,X_train=list("X_train_nn"=preds),  type="MLP",
-#'            link="identity",tau=0.8,n.ep=1000,
-#'                       batch.size=50, widths=c(6,3), seed=1)
+#' u_train <- u
 #'
-#' out<-predict_quant_nn(X_train=list("X_train_nn"=preds),model)
-#' u_train <- out$predictions
-#' print(mean(Y_train[Y_train> -1e5]>u_train[Y_train> -1e5]))
+#' #Fit the bGEV-PP model using u_train
+#' model<-bGEVPP.NN.train(Y_train, Y_test,X_train_q,X_train_s, u_train, type="MLP",link.loc="identity",
+#'                        n.ep=500, batch.size=50,init.loc=2, init.spread=2,init.xi=0.1,
+#'                        widths=c(6,3),seed=1, n_b=12)
+#' out<-bGEVPP.NN.predict(X_train_q=X_train_q,X_train_s=X_train_s,u_train=u_train,model)
 #'
-#' #Fit the bGEV-PP using u_train as output from quantile estimation model
-#' model2<-bGEVPP.NN.train(Y_train, Y_test,X_train_q,X_train_s, u_train, type="MLP",link.loc="identity",
-#'                         n.ep=500, batch.size=50,init.loc=2, init.spread=2,init.xi=0.1,
-#'                         widths=c(6,3),seed=1, n_b=12)
-#' out2<-bGEVPP.NN.predict(X_train_q=X_train_q,X_train_s=X_train_s,u_train=u_train,model2)
-#'
-#' print("q_alpha linear coefficients: "); print(round(out2$lin.coeff_q,2))
-#' print("s_beta linear coefficients: "); print(round(out2$lin.coeff_s,2))
+#' print("q_alpha linear coefficients: "); print(round(out$lin.coeff_q,2))
+#' print("s_beta linear coefficients: "); print(round(out$lin.coeff_s,2))
 #'
 #' #To save model, run
-#' #model %>% save_model_tf(paste0("model_bGEVPP"))
+#' #model %>% save_model_tf("model_bGEVPP")
 #' #To load model, run
-#' # model  <- load_model_tf(paste0("model_bGEVPP"),
+#' # model  <- load_model_tf("model_bGEVPP",
 #' #custom_objects=list("bgev_PP_loss_alpha__beta__p_a__p_b_"=bgev_PP_loss(n_b=12)))
 #'
 #' #Note that bGEV_PP_loss() can take custom alpha,beta, p_a and p_b arguments if defaults not used
@@ -225,7 +226,7 @@
 #'   for(j in 1:n.knot.q){
 #'     tmp[,j]=rad(plt.x,knots.q[i,j]) #Evaluate radial basis function of plt.x and all knots
 #'   }
-#'   plt.y=tmp%*%out2$gam.weights_q[i,]
+#'   plt.y=tmp%*%out$gam.weights_q[i,]
 #'   plot(plt.x,plt.y,type="l",main=paste0("q_alpha spline: predictor ",i),xlab="x",ylab="f(x)")
 #'   points(knots.q[i,],rep(mean(plt.y),n.knot.q),col="red",pch=2)
 #'   #Adds red triangles that denote knot locations
@@ -242,14 +243,12 @@
 #'   for(j in 1:n.knot.s){
 #'     tmp[,j]=rad(plt.x,knots.s[i,j]) #Evaluate radial basis function of plt.x and all knots
 #'   }
-#'   plt.y=tmp%*%out2$gam.weights_s[i,]
+#'   plt.y=tmp%*%out$gam.weights_s[i,]
 #'   plot(plt.x,plt.y,type="l",main=paste0("s_beta spline: predictor ",i),xlab="x",ylab="f(x)")
 #'   points(knots.s[i,],rep(mean(plt.y),n.knot.s),col="red",pch=2)
 #'   #Adds red triangles that denote knot locations
 #'
 #' }
-#'
-
 #'
 #' @rdname bGEVPP.NN
 #' @export
@@ -307,6 +306,7 @@ bGEVPP.NN.train=function(Y_train, Y_test = NULL,X_train_q,X_train_s, u_train = N
 
   if(!is.null(seed)) tf$random$set_seed(seed)
 
+  if(length(dim(u_train))!=length(dim(Y_train))+1) dim(u_train)=c(dim(u_train),1)
   model<-bGEVPP.NN.build(X_train_nn_q,X_train_lin_q,X_train_add_basis_q,
                          X_train_nn_s,X_train_lin_s,X_train_add_basis_s,
                          u_train,type, init.loc,init.spread,init.xi, widths,filter.dim,link.loc,alpha,beta,p_a,p_b)
@@ -433,7 +433,7 @@ bGEVPP.NN.build=function(X_train_nn_q,X_train_lin_q,X_train_add_basis_q,
 
 
 
-  if(lin.loc=="exp") init.loc=log(init.loc) else if(link.loc =="identity") init.loc=init.loc else stop("Invalid link function for location parameter")
+  if(link.loc=="exp") init.loc=log(init.loc) else if(link.loc =="identity") init.loc=init.loc else stop("Invalid link function for location parameter")
   #NN towers
 
   #Location
