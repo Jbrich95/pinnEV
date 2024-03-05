be372c1c07cdf2d4fa0c537066a206fb1ae0b925
Jordan Richards <jordan.richards@kaust.edu.sa>
2024-03-05 16:15:35 +0000

    Updated the USWild data
    Uploaded the new USWild data used by Richards and Huser (2024)

--- R/USWild.R
+++ R/USWild.R
@@ -6,60 +6,65 @@
 #'The response data \code{BA} are observations of monthly aggregated burnt area (acres) of 3503 spatial grid-cells 
 #'located across the contiguous United States, with the states of Alaska and Hawaii excluded.
 #'The observation period covers 1993 to 2015, using only months between March and September, inclusive, 
-#'leaving 161 observed spatial fields. Grid-cells are arranged on a regular latitude/longitude grid with spatial resolution
+#'leaving 161 observed spatial fields. Grid-cells are arranged on a regular 129 by 61 latitude/longitude grid with spatial resolution
 #'0.5deg by 0.5deg. Observations are provided by the Fire Program Analysis fire-occurrence database (Short, 2017)
 #'which collates U.S. wildfire records from the reporting systems of federal, state and local organisations.
 #'
 #'Both the response data and the subsequently described predictors have been re-gridded to a regular spatio-temporal grid with missing values set to \code{-1e10}.
 #'For \code{BA} and entries to \code{X}, the first three dimensions correspond to time \eqn{\times} latitude \eqn{\times} longitude with 
-#'their respective ordinate values given in \code{times}, \code{lat} and \code{lon}.
+#'their respective ordinate values given in \code{times}, \code{lat}, and \code{lon}.
 #'
-#'We consider three types of predictor variables, given in \code{X}: these are meteorological (\code{X.met}), land cover proportions (\code{X.lc}) and 
-#'orograhical (\code{X.oro}).
+#'We consider three types of predictor variables, given in \code{X}: these are the two "interpretable" predictors, 2m air temperature (K; \code{X.t2m}) and the standardised precipitation index (SPI; unitless; \code{X.SPI}), that Richards and Huser (2024) model using linear and additive functions, and the 40
+#' non-interpretable predictors (\code{X.N}) that feed the neural network component of the models. 
 #'
-#'Ten meteorological variables are considered and given as monthly means in \code{X.met}. 
-#'These variables are provided by the ERA5-reanalysis on land surface, available through the COPERNICUS Climate Data Service, 
-#'which is given on a 0.1deg \eqn{\times} 0.1deg grid; the values are then aggregated to a 0.5deg \eqn{\times} 0.5deg resolution.
-#'The variables are ordered as followed: both eastern and northern components of wind velocity at a 10m altitude (m/s), 
-#'both dew-point temperature and temperature at a 2m altitude (Kelvin), potential evaporation (m), evaporation (m), precipitation (m),
-#'surface pressure (Pa) and surface net solar, and thermal, radiation (J/m^2). 
-#'This particular ERA5-reanalysis samples over land only, and so the meteorological conditions over the 
-#'oceans are not available.
+#'For \code{X.N}, the names of each variable are contained with \code{X$cov.names}.
+#'These include 11 meteorological variables, which are given as monthly means.
+#'These variables are provided by the ERA5-reanalysis on single levels, available through the COPERNICUS Climate Data Service  (\href{https://doi.org/10.24381/cds.f17050d7}{doi:10.24381/cds.f17050d7}), 
+#'which is given on a 0.1deg \eqn{\times} 0.1deg grid; the values have then been aggregated to a 0.5deg \eqn{\times} 0.5deg resolution.
+#'The variables are: eastern (u10; m/s) and northern (v10; m/s) components of wind velocity at 10m above ground level, 
+#'potential evaporation (pev; m), evaporation (e; m of water equivalent), surface pressure (sp; Pa),
+#'surface net solar (ssr; J/m^2) and thermal radiation (str; J/m^2), snowfall (sf; m of water equivalent), 
+#'snow-melt (smlt; m of water equivalent), snow evaporation (es; m of water equivalent), and
+#'surface run-off (sro; m). The reanalysis also provides three variables relating to sub-gridscale orography: angle (anor; radians), 
+#'anisotropy (isor; unitless), slope (slor; unitless), as well as the standard deviation of orography (sdor; unitless).
 #'
-#'The land cover variables that are given in \code{X.lc} describe the proportion of a grid-cell which
-#'is covered by one of 18 different types, e.g., urban, grassland, water (see Opitz (2022) for full details).
+#'We also provide land cover variables in \code{X.N} that describe the proportion of a grid-cell which
+#'is covered by one of 25 different types, e.g., urban, grassland, water (see Opitz (2022) for details on a subset of these data).
 #'Land cover predictors are derived using a gridded land cover map, of spatial resolution 300m and temporal resolution one year, 
 #'produced by COPERNICUS and available through their Climate Data Service. For each 0.5deg \eqn{\times} 0.5deg grid-cell, 
 #'the proportion of a cell consisting of a specific land cover type is derived from the high-resolution product.
-#'The variables are ordered lc1 - lc18, as described by Opitz (2022).
+#'The land cover variables names are provided explicitly in \code{cov.names}.
 #'
-#'The two orographical predictors given in \code{X.oro} are the mean and standard deviation of the altitude (m) 
-#'for each grid-cell; estimates are derived using a densely-sampled gridded 
-#'output from the U.S. Geographical Survey Elevation Point Query Service. 
+#'
+#'Also provided are \code{state.flags} and \code{state.IDs}, which provide the state in which each of the (129 by 61) spatial grid-cells are located. A value of zero corresponds to a spatial grid-cell located outside of the CONUS. Note that some of the smaller states have been merged.
 #' @docType data
 #'
 #' @usage data(USWild)
 #'
-#' @format A list with 5 elements:
+#' @format A list with 7 elements:
 #' \describe{
-#' \item{BA}{An array with dimension (276, 119, 51), corresponding to the burnt area response data.}
-#' \item{X}{A list with 3 elements:
+#' \item{BA}{An array with dimension (161, 129, 61), corresponding to the burnt area response data.}
+#' \item{X}{A list with 4 elements:
 #' \describe{
-#' \item{X.met}{An array with dimension (276, 119, 51, 10) corresponding to the meterological predictors described below.}
-#'  \item{X.lc}{An array with dimension (276, 119, 51, 18) corresponding to the land cover predictors described below.}
-#' \item{X.oro}{An array with dimension (276, 119, 51, 2) corresponding to the oropgraphical predictors described below.}
+#' \item{X.t2m}{An array with dimension (161, 129, 61, 1) of 2m air temperature values, as described below.}
+#'  \item{X.SPI}{An array with dimension (161, 129, 61, 1) of standardised precipitation index (SPI) values, as described below.}
+#' \item{X.N}{An array with dimension (161, 129, 61, 40) of the "non-interpretable" neural network predictors, as described below.}
+#' \item{cov.names}{A vector of length 40 giving shorthand names for the covariates that correspond to the last dimension of \code{X.N}. See below.}
 #'
 #' }
 #' }
-#' \item{times}{A vector of length 276 giving the observation indices. Format is "year-month". Corresponds to first dimension of \code{BA}.}
-#'  \item{lon}{A vector of length 119 giving the longitude ordinate for the second dimension of \code{BA}.}
-#'  \item{lat}{A vector of length 51 giving the latitude ordinate for the third dimension of \code{BA}.}
+#' \item{times}{A vector of length 161 giving the monthly observation indices. Format is "year-month". Corresponds to first dimension of \code{BA}.}
+#'  \item{lon}{A vector of length 129 giving the longitude ordinate for the second dimension of \code{BA}.}
+#'  \item{lat}{A vector of length 61 giving the latitude ordinate for the third dimension of \code{BA}.}
+#'   \item{state.ids}{A (129,61) matrix providing the state ID number for each spatial grid-cell. A value of 0 corresponds to grid-cells outside of the CONUS, e.g., over sea.}
+#'  \item{state.flags}{A vector of length 45 providing the name associated with each state ID.}
+
 
 #' }
 #'
 #' @keywords datasets
 #'
-#'@references Richards, J. and Huser, R. (2022), \emph{Regression modelling of spatiotemporal extreme U.S. wildfires via partially-interpretable neural networks}. (\href{https://arxiv.org/abs/2208.07581}{arXiv:2208.07581}).
+#'@references Richards, J. and Huser, R. (2024+), \emph{Regression modelling of spatiotemporal extreme U.S. wildfires via partially-interpretable neural networks}. (\href{https://arxiv.org/abs/2208.07581}{arXiv:2208.07581}).
 #'
 #'Short, K. C. (2017), \emph{Spatial wildfire occurrence data for the United States, 1992-2015 [FPA_FOD_20170508].  
 #'4th Ed. Fort Collins, CO: Forest Service Research Data Archive.}
